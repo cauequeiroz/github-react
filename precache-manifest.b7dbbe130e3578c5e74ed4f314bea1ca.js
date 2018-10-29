ce0913cdb35ef528cbdd5c788c1ee04579a79985
Caue Queiroz <cauenqueiroz@gmail.com>
2018-10-29 01:50:02 -0300

    Updates

---
+++ precache-manifest.b7dbbe130e3578c5e74ed4f314bea1ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "23fd3a8e675fa70ad80c",
+    "url": "/github-react/static/js/runtime~main.23fd3a8e.js"
+  },
+  {
+    "revision": "dea7523a0bf5b6b6cf97",
+    "url": "/github-react/static/js/main.dea7523a.chunk.js"
+  },
+  {
+    "revision": "b8ad3912fd06329b8dc1",
+    "url": "/github-react/static/js/1.b8ad3912.chunk.js"
+  },
+  {
+    "revision": "dea7523a0bf5b6b6cf97",
+    "url": "/github-react/static/css/main.810eee5a.chunk.css"
+  },
+  {
+    "revision": "c3b8e2cd464af8c3f7af3b890725a9d4",
+    "url": "/github-react/index.html"
+  }
+];
